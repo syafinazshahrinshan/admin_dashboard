65a23c3f382354666151a4bf3566e837379c4c97
syafinazshahrinshan <89062054+syafinazshahrinshan@users.noreply.github.com>
2022-03-27 00:07:28 +0800

    publish admin dashboard

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js
+
+module.exports = {
+    devServer: {
+        // This will forward any request that does not match a static file to localhost:3000
+        proxy: 'http://localhost:3050'
+    }
+}
