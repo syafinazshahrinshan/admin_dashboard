8bd33c72693da9868f7c6ddf4ff51c19371ed8e5
syafinazshahrinshan <89062054+syafinazshahrinshan@users.noreply.github.com>
2022-03-27 16:30:22 +0800

    fix post request

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+// const cors = require('cors')
+// app.use(
+//     cors({
+//         origin:"*",
+//         methods:['GET','POST']
+//     })
+// )
 app.listen(3000,()=>{
     console.log("server is listening on port 3000")
 })
